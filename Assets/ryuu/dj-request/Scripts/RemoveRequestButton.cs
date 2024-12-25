0342e9200f1d1025579226bff3e6d7eeffa3a743
ryuunosuke <ryuu@waifu.club>
2024-12-25 20:12:25 +0300

    Add request removal. Change UI slightly.

---
+++ Assets/ryuu/dj-request/Scripts/RemoveRequestButton.cs
@@ -0,0 +1,77 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+
+namespace ryuu
+{
+    public class RemoveRequestButton : UdonSharpBehaviour
+    {
+        public int index;
+        public RequestController requestController;
+
+        float _animation = 0f;
+        RectTransform _parentrect;
+        float _width;
+        float _height;
+
+        private void Start()
+        {
+            _parentrect = transform.parent.GetComponent<RectTransform>();
+            _width = _parentrect.sizeDelta.x;
+            _height = _parentrect.sizeDelta.y;
+        }
+        public void onClick()
+        {
+            if (requestController == null)
+            {
+                Debug.LogError("[RyuuRequest] Request controller was not set!");
+                return;
+            }
+            if (!requestController.animateDelete)
+            {
+                requestController.removeRequest(index);
+            }
+            else
+            {
+                updateAnimation();
+            }
+        }
+
+        float EaseOutPower(float x, float pow)
+        {
+            return 1 - Mathf.Pow(1 - x, pow);
+        }
+
+        float EaseInOutPower(float x, float pow)
+        {
+            return Mathf.Lerp(EaseInPower(x, pow), EaseOutPower(x, pow), x);
+        }
+
+        float EaseInPower(float x, float pow)
+        {
+            return Mathf.Pow(x, pow);
+        }
+
+        public void updateAnimation()
+        {
+            float duration = 0.275f;
+            
+            float width_multiplier = EaseInOutPower(Mathf.Clamp(_animation * 2, 0, 1f), 3);
+            float height_multiplier = EaseInOutPower(Mathf.Clamp((_animation - .5f) * 2, 0, 1f), 3);
+
+            if (_animation < 1f)
+            {
+                _animation += 1f / (60f * duration);
+                _parentrect.sizeDelta = new Vector2(Mathf.Lerp(_width, 0, width_multiplier), Mathf.Lerp(_height, -38, height_multiplier));
+                SendCustomEventDelayedSeconds(nameof(updateAnimation), 1f / 60f);
+                return;
+            }
+            else
+            {
+                _animation = 0f;
+                requestController.removeRequest(index);
+            }
+        }
+    }
+}
--- Assets/ryuu/dj-request/Scripts/RequestController.cs
+++ Assets/ryuu/dj-request/Scripts/RequestController.cs
@@ -2,164 +2,238 @@
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
-using VRC.Udon;
 using System;
 
-[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
-public class RequestController : UdonSharpBehaviour {
+namespace ryuu
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class RequestController : UdonSharpBehaviour
+    {
 
 
-    [Header("Options")]
-    [SerializeField]
-    [Min(0)]
-    [Tooltip("Seconds until user can submit another request.")]
-    // Seconds!
-    int requestCooldown = 180;
+        [Header("Options")]
+        [SerializeField]
+        [Min(0)]
+        [Tooltip("Seconds until user can submit another request.")]
+        // Seconds!
+        int requestCooldown = 90;
 
-    [Space(10)]
-    [Header("Internal")]
+        [Tooltip("Animate deletion of requests.")]
+        public bool animateDelete = false;
 
-    DateTime lastRequestTime;
+        [Space(10)]
+        [Header("Internal")]
 
-    [Space(5)]
-    [Header("Client")]
+        DateTime lastRequestTime;
 
-    [SerializeField]
-    UnityEngine.UI.InputField requestInputField;
+        [Space(5)]
+        [Header("Client")]
 
-    [SerializeField]
-    UnityEngine.UI.Text submitButton;
+        [SerializeField]
+        UnityEngine.UI.InputField requestInputField;
 
-    [Header("Display")]
+        [SerializeField]
+        UnityEngine.UI.Text submitButton;
 
-    [SerializeField]
-    GameObject contentParent;
+        [Header("Display")]
 
-    [SerializeField]
-    GameObject listTilePrefab;
+        [SerializeField]
+        GameObject contentParent;
 
-
-    [Header("Admin")]
-
-    [SerializeField]
-    GameObject worldUI;
+        [SerializeField]
+        GameObject listTilePrefab;
 
 
-    [UdonSynced]
-    public bool synced_requestsEnabled;
+        [Header("Admin")]
 
-    [UdonSynced]
-    [HideInInspector]
-    public string synced_requests = "";
+        [SerializeField]
+        GameObject worldUI;
 
 
-    void Start() {
-        lastRequestTime = DateTime.Now.AddMinutes(-requestCooldown);
-        createList();
-    }
+        [UdonSynced]
+        public bool synced_requestsEnabled;
 
-    private void Update() {
-        string buttonText;
-        double secondsTillRequest = requestCooldown - (DateTime.Now - lastRequestTime).TotalSeconds;
-        if (secondsTillRequest < 0) {
-            buttonText = "Submit";
-        } else {
-            buttonText = string.Format("{0:00}:{1:00}", Math.Floor(secondsTillRequest / 60), Math.Floor(secondsTillRequest % 60));
+        [UdonSynced]
+        [HideInInspector]
+        public string synced_requests = "";
+
+
+        void Start()
+        {
+            lastRequestTime = DateTime.Now.AddMinutes(-requestCooldown);
+            createList();
         }
-        submitButton.text = buttonText;
-    }
 
-    public void createList() {
-        // ---- DECODE LOGIC! ----
-        int i = 0;
-        int k = 0;
-        string[] requestData = new string[2];
-        int dataCount = 0;
-        int requestIndex = 1;
-        while (k < synced_requests.Length) {
-            if (synced_requests[k] == '>') {
-                int stringLength = int.Parse(synced_requests.Substring(i, k - i));
-                string parsingString = synced_requests.Substring(k + 1, stringLength);
-                requestData[dataCount] = parsingString;
-                i = k + stringLength + 1;
-                k = i;
-                dataCount += 1;
+        private void Update()
+        {
+            string buttonText;
+            double secondsTillRequest = requestCooldown - (DateTime.Now - lastRequestTime).TotalSeconds;
+            if (secondsTillRequest < 0)
+            {
+                buttonText = "送る";
             }
-            k++;
+            else
+            {
+                buttonText = string.Format("{0:00}:{1:00}", Math.Floor(secondsTillRequest / 60), Math.Floor(secondsTillRequest % 60));
+            }
+            submitButton.text = buttonText;
+        }
 
-            if (dataCount == 2) {
-                dataCount = 0;
-                // Generate list tile
-                GameObject listTile = Instantiate(listTilePrefab, contentParent.transform);
-                listTile.transform.Find("Index").GetComponent<UnityEngine.UI.Text>().text = requestIndex.ToString();
-                listTile.transform.Find("Player Name").GetComponent<UnityEngine.UI.Text>().text = requestData[0];
-                listTile.transform.Find("Request").GetComponent<UnityEngine.UI.Text>().text = requestData[1];
-                listTile.transform.SetAsFirstSibling();
-                requestIndex += 1;
+        public void createList()
+        {
+            // ---- DECODE LOGIC! ----
+            int i = 0;
+            int k = 0;
+            string[] requestData = new string[2];
+            int dataCount = 0;
+            int requestIndex = 1;
+            while (k < synced_requests.Length)
+            {
+                if (synced_requests[k] == '>')
+                {
+                    int stringLength = int.Parse(synced_requests.Substring(i, k - i));
+                    string parsingString = synced_requests.Substring(k + 1, stringLength);
+                    requestData[dataCount] = parsingString;
+                    i = k + stringLength + 1;
+                    k = i;
+                    dataCount += 1;
+                }
+                k++;
+
+                if (dataCount == 2)
+                {
+                    dataCount = 0;
+                    // Generate list tile
+                    GameObject listTile = Instantiate(listTilePrefab, contentParent.transform);
+                    listTile.transform.Find("Index").GetComponent<UnityEngine.UI.Text>().text = $"#{requestIndex.ToString()}";
+                    listTile.transform.Find("Player Name").GetComponent<UnityEngine.UI.Text>().text = requestData[0];
+                    listTile.transform.Find("Request").GetComponent<UnityEngine.UI.Text>().text = requestData[1];
+                    GameObject deleteButton = listTile.transform.Find("Delete").gameObject;
+                    deleteButton.GetComponent<RemoveRequestButton>().requestController = this;
+                    deleteButton.GetComponent<RemoveRequestButton>().index = requestIndex;
+
+                    listTile.transform.SetAsFirstSibling();
+                    requestIndex += 1;
+                }
+            }
+            // ------- DECODE ---------
+        }
+
+        public void wipeList()
+        {
+            for (int i = 0; i < contentParent.transform.childCount; i++)
+            {
+                Destroy(contentParent.transform.GetChild(i).gameObject);
             }
         }
-        // ------- DECODE ---------
-    }
 
-    public void wipeList() {
-        for (int i = 0; i < contentParent.transform.childCount; i++) {
-            Destroy(contentParent.transform.GetChild(i).gameObject);
+        public void addRequest()
+        {
+            if (requestInputField.text.Length == 0)
+            {
+                return;
+            }
+            if ((DateTime.Now - lastRequestTime).TotalSeconds < requestCooldown)
+            {
+                return;
+            }
+            string encodedText = "";
+
+            encodedText += $"{Networking.LocalPlayer.displayName.Length}>{Networking.LocalPlayer.displayName}";
+            encodedText += $"{requestInputField.text.Length}>{requestInputField.text}";
+
+            requestInputField.text = "";
+
+            if (!Networking.IsOwner(gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            synced_requests += encodedText;
+            lastRequestTime = DateTime.Now;
+
+            RequestSerialization();
+            OnDeserialization();
         }
-    }
 
-    public void addRequest() {
-        if (requestInputField.text.Length == 0) {
-            return;
+        public void removeRequest(int remove_index)
+        {
+            if (!Networking.IsOwner(gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            int i = 0;
+            int k = 0;
+            string[] requestData = new string[2];
+            int dataCount = 0;
+            int requestIndex = 0;
+            int remove_start = 0;
+            int remove_end = 0;
+            while (k < synced_requests.Length)
+            {
+                if (synced_requests[k] == '>')
+                {
+                    int stringLength = int.Parse(synced_requests.Substring(i, k - i));
+                    string parsingString = synced_requests.Substring(k + 1, stringLength);
+                    requestData[dataCount] = parsingString;
+                    i = k + stringLength + 1;
+                    k = i;
+                    dataCount += 1;
+                }
+                if (dataCount == 2)
+                {
+                    dataCount = 0;
+                    requestIndex += 1;
+                    remove_end = k;
+                }
+                if (requestIndex == remove_index && dataCount == 0)
+                {
+                    remove_start = k - (requestData[0].Length + requestData[0].Length.ToString().Length + requestData[1].Length + requestData[1].Length.ToString().Length + 2);
+                    remove_end = k;
+                    break;
+                }
+                k++;
+            }
+            Debug.Log($"[RyuuRequest] Removed request {synced_requests.Substring(remove_start, remove_end - remove_start)}");
+            synced_requests = synced_requests.Substring(0, remove_start) + synced_requests.Substring(remove_end);
+            RequestSerialization();
+            OnDeserialization();
         }
-        if ((DateTime.Now - lastRequestTime).TotalSeconds < requestCooldown) {
-            return;
+        public void clearRequests()
+        {
+            synced_requests = "";
+            wipeList();
+            if (!Networking.IsOwner(gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            RequestSerialization();
+            OnDeserialization();
         }
-        string encodedText = "";
 
-        encodedText += $"{Networking.LocalPlayer.displayName.Length}>{Networking.LocalPlayer.displayName}";
-        encodedText += $"{requestInputField.text.Length}>{requestInputField.text}";
-
-        requestInputField.text = "";
-
-        if (!Networking.IsOwner(gameObject)) {
-            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        // enables/disables requests depending on `synced_requestsEnabled`.
+        void updateUserAllowState()
+        {
+            worldUI.SetActive(synced_requestsEnabled);
         }
-        synced_requests += encodedText;
-        lastRequestTime = DateTime.Now;
 
-        RequestSerialization();
-        OnDeserialization();
-    }
-
-    public void clearRequests() {
-        synced_requests = "";
-        wipeList();
-        if (!Networking.IsOwner(gameObject)) {
-            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        public void toggleWorldInterface()
+        {
+            synced_requestsEnabled = !synced_requestsEnabled;
+            if (!Networking.IsOwner(gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            RequestSerialization();
+            OnDeserialization();
         }
-        RequestSerialization();
-        OnDeserialization();
-    }
 
-    // enables/disables requests depending on `synced_requestsEnabled`.
-    void updateUserAllowState() {
-        worldUI.SetActive(synced_requestsEnabled);
-    }
-
-    public void toggleWorldInterface() {
-        synced_requestsEnabled = !synced_requestsEnabled;
-        if (!Networking.IsOwner(gameObject)) {
-            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        public override void OnDeserialization()
+        {
+            wipeList();
+            createList();
+            updateUserAllowState();
         }
-        RequestSerialization();
-        OnDeserialization();
+
     }
-
-
-    public override void OnDeserialization() {
-        wipeList();
-        createList();
-        updateUserAllowState();
-    }
-
 }
